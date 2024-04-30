c369cb15d2d833e71ff2315fada39aee55d6d36d
Andualem Abebaw <andyabebaw1@gmail.com>
2024-04-30 00:46:09 -0500

    initial setup
    get the basics setup

--- Gively/App.js
+++ Gively/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.jsss to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
