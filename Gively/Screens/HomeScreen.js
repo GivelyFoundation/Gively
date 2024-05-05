5cd5f5801669d5cd41cf550387e5a590822de369
Andualem Abebaw <andyabebaw1@gmail.com>
2024-05-04 21:36:24 -0500

    add switch selector for foryou/friends feeds
    adds switch selector to the home page to switch between the foryou feed and the friends feed

--- Gively/Screens/HomeScreen.js
+++ Gively/Screens/HomeScreen.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SwitchSelector from "react-native-switch-selector";
 import styles from '../Styles.js/Styles';
 
+
+const ForYouFeed = () => {
+  return (
+    <View style={styles.container}>
+      <Text>{`For You Feed`}</Text>
+    </View>
+  );
+};
+
+const FriendsFeed = () => {
+  return (
+    <View style={styles.container}>
+      <Text>{`Friends Feed`}</Text>
+    </View>
+  );
+};
+
 export default function HomeFeedScreen({ navigation }) {
-      const handleLogout = () => {
-        navigation.navigate('Login');
-      };
-      return (
-        <View style={styles.container}>
-          <Text>Welcome to the Home Screen!</Text> 
-        </View>
-      );
+  const [activeTab, setActiveTab] = useState('For You');
+
+  // Update activeTab based on the selected value from SwitchSelector
+  const handleTabPress = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <View style={styles.container}>
+      <SwitchSelector
+        initial={0}
+        onPress={value => handleTabPress(value)}
+        hasPadding
+        options={[
+          { label: "For You", value: "For You" },
+          { label: "Friends", value: "Friends" }
+        ]}
+        testID="feed-switch-selector"
+        accessibilityLabel="feed-switch-selector"
+      />
+      {activeTab === 'For You' ? < ForYouFeed /> : <FriendsFeed />}
+    </View>
+  );
 }
