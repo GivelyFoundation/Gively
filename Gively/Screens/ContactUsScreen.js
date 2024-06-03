430f66ba534baa31c9aa37fb8063ae6589c9dcc8
Andualem Abebaw <andyabebaw1@gmail.com>
2024-06-03 00:04:09 -0500

    update support email
    update support email

--- Gively/Screens/ContactUsScreen.js
+++ Gively/Screens/ContactUsScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native'
 const ContactUsScreen = () => {
   // Function to handle email press
   const handleEmailPress = () => {
-    const email = "info@gively.com";
+    const email = "support@gively.com";
     Linking.openURL(`mailto:${email}`);
   };
 
@@ -13,7 +13,7 @@ const ContactUsScreen = () => {
       <Text style={[styles.header, {fontFamily: 'Montserrat-Medium'}]}>Contact Us</Text>
 
       <TouchableOpacity style={styles.button} onPress={handleEmailPress}>
-        <Text style={[styles.buttonText,{fontFamily: 'Montserrat-Medium'}]}>Email Us: info@gively.com</Text>
+        <Text style={[styles.buttonText,{fontFamily: 'Montserrat-Medium'}]}>Email Us: support@gively.com</Text>
       </TouchableOpacity>
     </View>
   );
